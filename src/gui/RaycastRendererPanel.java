85b71100ac21476c5d27b090e93616493d8fe4a4
daleili <lidalei151781@gmail.com>
2015-11-26 11:41:05 +0100

    RayCastRendererPanel.java update compositing event listener

--- src/gui/RaycastRendererPanel.java
+++ src/gui/RaycastRendererPanel.java
@@ -133,18 +133,18 @@ public class RaycastRendererPanel extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void mipButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mipButtonActionPerformed
-        renderer.Method_Implemented="MIP";
-        JOptionPane.showMessageDialog(this, renderer.Method_Implemented);
+        this.renderer.Method_Implemented="MIP";
+        // JOptionPane.showMessageDialog(this, renderer.Method_Implemented);
 
     }//GEN-LAST:event_mipButtonActionPerformed
 
     private void slicerButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_slicerButtonActionPerformed
-        renderer.Method_Implemented="Slicer";
-        JOptionPane.showMessageDialog(this, renderer.Method_Implemented);
+        this.renderer.Method_Implemented="Slicer";
+        // JOptionPane.showMessageDialog(this, renderer.Method_Implemented);
     }//GEN-LAST:event_slicerButtonActionPerformed
 
     private void compositingButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_compositingButtonActionPerformed
-        JOptionPane.showMessageDialog(this, "Not implemented.");
+        this.renderer.Method_Implemented="Compositing";
     }//GEN-LAST:event_compositingButtonActionPerformed
 
     private void tf2dButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tf2dButtonActionPerformed
